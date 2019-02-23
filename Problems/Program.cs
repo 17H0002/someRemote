f4af7202a31e371647226f3aca1babee3038127b
Matt <matt2hagemann@gmail.com>
2019-02-23 19:56:09 +0200

    Solution, problem, tests

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,37 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
+
+        public static string Prefix(string input)
+        {
+            if(input == null)
+            {
+                return null;
+            }
+
+            else
+            {
+            int x = input.Length;
+
+            string[] words = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            
+
+
+            string ans = $"{x},{words.Length}:{input}";
+            return ans;
+            }
+
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string test = "what ... did you say??";
+            Console.WriteLine(Prefix(test));
         }
     }
+
+    
 }
